5e5bb76b371b07c7770d422f33395a8f9003d0ea
yeaseul7 <104067367+yeaseul7@users.noreply.github.com>
2022-07-15 13:56:30 +0900

    addAppend

---
+++ week3/append.py
@@ -0,0 +1,15 @@
+fruit=['banana','grapes','apple']
+fruit.append('mango')
+print(fruit)
+#['banana', 'grapes', 'apple', 'mango']
+
+# 빈 리스트 line_up이 있어요
+line_up = []
+
+# 거북이, 홍학, 토끼 순으로 도착했어요. 차례로 추가해 보세요
+line_up.append('거북이')
+line_up.append('홍학')
+line_up.append('토끼')
+
+# 줄을 잘 세웠는지 line_up을 출력해 확인해요
+print(line_up)
